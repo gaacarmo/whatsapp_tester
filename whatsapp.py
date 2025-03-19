027cfbe194f58aca08f6f935b829444d1b99c030
Gabriel Carmo <gabriel.carmo@salvy.com.br>
2025-03-19 15:02:18 -0300

    Eliminação de falsos positivos

--- whatsapp.py
+++ whatsapp.py
@@ -4,7 +4,9 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException, WebDriverException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, WebDriverException
 
 # Configuração do Selenium para Chrome
 options = webdriver.ChromeOptions()
@@ -19,9 +21,6 @@ navegador = webdriver.Chrome(service=servico, options=options)
 try:
     # Abrir WhatsApp Web
     navegador.get('https://web.whatsapp.com/')
-    navegador.implicitly_wait(5.0)
-
-    # Pedir para o usuário fazer login
     print('-----------------------------------')
     print('ATENÇÃO: FAÇA O LOGIN NO WHATSAPP WEB E TECLE ENTER')
     print('-----------------------------------')
@@ -39,17 +38,27 @@ try:
         for numero in numeros:
             url = f'https://web.whatsapp.com/send/?phone=55{numero}&text&type=phone_number'
             navegador.get(url)
-            navegador.implicitly_wait(10.0)
+
+            try:
+                # Espera explícita para garantir que a página seja carregada completamente
+                WebDriverWait(navegador, 20).until(
+                    EC.presence_of_element_located((By.TAG_NAME, 'body'))
+                )
+            except TimeoutException:
+                print(f"Timeout ao carregar a página para o número: {numero}")
+                status = "ERRO"
+                resultados.append(f"Numero: {numero} - Status: {status}\n")
+                continue
 
             # Pausa aleatória para evitar bloqueios
-            time.sleep(random.uniform(3, 7))
+            time.sleep(random.uniform(5, 10))
 
             # Obtém o texto da página
             page_text = navegador.page_source
-    
+
             # Verifica se a mensagem de erro indicando que o número não está no WhatsApp está presente
             if "O número de telefone compartilhado por url é inválido." in page_text:
-                status = "NAO WHATSAPP"
+                status = "NAO"
                 negativo += 1
             else:
                 status = "OK"
